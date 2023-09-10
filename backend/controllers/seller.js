59a05843dbbf476993167430606ac4af21634c6d
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-09-10 21:20:55 +0530

    updateQuantity is done using an easier way.

--- backend/controllers/seller.js
+++ backend/controllers/seller.js
@@ -31,25 +31,13 @@ exports.updateQuantity = async (req, res) => {
   try {
     const candyId = req.params.id;
     const buyNumber = req.params.buy;
-    const name = req.body.name;
-    const description = req.body.description;
-    const price = req.body.price;
     const quantity = req.body.quantity - buyNumber;
-
     if(quantity < 0) {
-        throw new Error('quantity is now under zero');
+      throw new Error('quantity is now under zero');
     }
-
-    const updatedEntry = await CandyInfo.update(
-      {
-        candyName: name,
-        description: description,
-        price: price,
-        quantity: quantity,
-      },
-      { where: { id: candyId } }
-    );
-
+    const data = await CandyInfo.findByPk(candyId);
+    data.quantity = quantity;
+    const updatedEntry = await data.save();
     res.status(200).json(updatedEntry);
   } catch (error) {
     res.status(500).json({
